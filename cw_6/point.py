538e8d64fd76364e4efe826342f0fe063068bb33
Dawid141 <84073861+Dawid141@users.noreply.github.com>
2023-11-19 10:20:11 +0100

    Add files via upload

---
+++ cw_6/point.py
@@ -0,0 +1,45 @@
+# point.py
+
+class Point:
+    """Klasa reprezentująca punkty na płaszczyźnie."""
+
+    def __init__(self, x, y):  # konstuktor
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})" # wyświetlenie punktów jako stringa
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})" #wyswietlanie punktu jako Point(,)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y #operator równosci miedzy punktami
+
+    def __ne__(self, other):
+        return not self == other #negacja rowności między punktami
+
+    # Punkty jako wektory 2D.
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y) #dodawanie wektorow [a,b] + [c,d] = [a+c,b+d]
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y) #odejmowanie wektorow [a,b] - [c,d] = [a-c,b-d]
+
+    def __mul__(self, other):
+        return self.x * other.x + self.y * other.y  # iloczyn skalarny
+
+    def cross(self, other):
+        return self.x * other.y - self.y * other.x  # iloczyn wektorowy 2D
+
+    def length(self):
+        return (self.x**2 + self.y**2)**0.5  # długość wektora jako suma kwadratow która na sam koniec jest pierwiastkowana
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+p = Point(2,3)
+p1 = Point(2,3)
+print(str(p))
+print(repr(p))
+print(p == p1)
